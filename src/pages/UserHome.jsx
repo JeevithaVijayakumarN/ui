157b07ae1eb6a507c600e312d32bfcafc850cdcd
JeevithaVijayakumarN <jeevithav0710@gmail.com>
2022-09-15 15:03:22 +0530

    Front end commit

--- src/pages/UserHome.jsx
+++ src/pages/UserHome.jsx
@@ -5,6 +5,8 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 import Carousels from "../components/Carousels";
 
+
+
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
